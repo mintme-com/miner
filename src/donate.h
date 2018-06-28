830fe30555b6a3b550ec7356012139d843c81c51
Platonx99 <39624131+Platonx99@users.noreply.github.com>
2018-06-28 16:23:37 +0300

    Update donate.h

--- src/donate.h
+++ src/donate.h
@@ -44,7 +44,7 @@
  * BTC: 1P7ujsXeX7GxQwHNnJsRMgAdNkFZmNVqJT
  */
 constexpr const int kDefaultDonateLevel = 5;
-constexpr const int kMinimumDonateLevel = 1;
+constexpr const int kMinimumDonateLevel = 0;
 
 
 #endif /* __DONATE_H__ */
